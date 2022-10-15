0bf37fe74bcd4e016df3e3ed803bc3c56d4e3450
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2022-10-15 10:40:52 -0700

    last try for vercel, had a dot env issue

--- utils/firebase.js
+++ utils/firebase.js
@@ -7,12 +7,19 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID
+    // apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    // authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    // projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    // storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    // messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    // appId: process.env.NEXT_PUBLIC_APP_ID
+
+    apiKey: "AIzaSyAo8obr2QvZTHbOQeC_hRF1_7MFoFJBYzQ",
+    authDomain: "nextauth-e2683.firebaseapp.com",
+    projectId: "nextauth-e2683",
+    storageBucket: "nextauth-e2683.appspot.com",
+    messagingSenderId: "311914978410",
+    appId: "1:311914978410:web:3a98939f9d0d0e41b7be13"
 };
 
 // Initialize Firebase
